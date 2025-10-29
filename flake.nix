bf490df474ba197292fc1b4a982521abeeda8c61
connerohnesorge <conneroisu@outlook.com>
2025-10-29 09:50:02 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  description = "flake for https://archil.com/ linux installables";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    treefmt-nix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ ];
+      };
+
+      rooted = exec:
+        builtins.concatStringsSep "\n"
+        [
+          ''REPO_ROOT="$(git rev-parse --show-toplevel)"''
+          exec
+        ];
+
+      scripts = {
+        dx = {
+          exec = rooted ''$EDITOR "$REPO_ROOT"/flake.nix'';
+          description = "Edit flake.nix";
+        };
+        gx = {
+          exec = rooted ''$EDITOR "$REPO_ROOT"/go.mod'';
+          description = "Edit go.mod";
+        };
+      };
+
+      scriptPackages =
+        pkgs.lib.mapAttrs
+        (
+          name: script:
+            pkgs.writeShellApplication {
+              inherit name;
+              text = script.exec;
+              runtimeInputs = script.deps or [];
+            }
+        )
+        scripts;
+
+      treefmtModule = {
+        projectRootFile = "flake.nix";
+        programs = {
+          alejandra.enable = true; # Nix formatter
+          gofmt.enable = true; # Go formatter
+          golines.enable = true; # Go formatter (Shorter lines)
+          goimports.enable = true; # Go formatter (Organize/Clean imports)
+        };
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "dev";
+
+        # Available packages on https://search.nixos.org/packages
+        packages = with pkgs;
+          [
+            alejandra # Nix
+            nixd
+            statix
+            deadnix
+
+          ]
+          ++ builtins.attrValues scriptPackages;
+      };
+
+      packages = {
+        # TODO: Build the main cli/client binary from fetched rpm
+      };
+
+      formatter = treefmt-nix.lib.mkWrapper pkgs treefmtModule;
+    });
+}
